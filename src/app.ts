1462c087c2aae0e1aca78e17d1ba921b45368aa7
Paulo Uilian <uilianlbarros@gmail.com>
2022-11-08 16:41:17 -0300

    :bug:fix: remove commented line

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,7 @@ import express, { Express, json } from 'express';
 import { productsRoute } from './routes/productsRoute';
 
 dotenv.config();
-//const time = process.env.TIME_DATABASE_UPDATE ?? '00:00:00';
-const t  = new Date();
-const time = `${t.getHours()}:${t.getMinutes()}:${t.getSeconds() + 5}`;
-console.log(time);
+const time = process.env.TIME_DATABASE_UPDATE ?? '00:00:00';
 const routine = new DatabaseUpdateRoutine(time);
 routine.start();
 
